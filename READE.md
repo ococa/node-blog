11407a09ea8dfedce881683673c35195aa7baf62
ococa <wangchao.personal@gmail.com>
2019-06-01 12:22:19 +0800

    init

---
+++ READE.md
@@ -0,0 +1,1 @@
+# blog-demo based node
---
+++ bin/www.js
@@ -0,0 +1,8 @@
+// server config
+const http = require('http');
+const serverHandle = require('../app');
+
+const PORT = 8000;
+
+const server = http.createServer(serverHandle);
+server.listen(PORT);
